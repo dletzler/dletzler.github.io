e6b39f9472eb4f1141bac9297e0d80be88294bc4
dletzler <david.letzler@gmail.com>
2017-04-04 01:00:29 -0400

    Put Project 3 on portfolio

---
+++ Renthop Kaggle/Dave_file_size.R
@@ -0,0 +1,34 @@
+library(RCurl)
+
+#Test size of online picture
+file.size<-function(url){
+  xx = getURL(url, nobody=1L, header=1L)
+  info = unlist(strsplit(xx, "\r\n"))
+  size<-unlist(strsplit(info[grepl("Content-Length", info)], split=" "))[2]
+  return(as.numeric(size))
+}
+
+#Get average size of set
+avg.size<-function(x){
+  sizes<-sapply(x,file.size)
+  print("Finished another")
+  return(sizes)
+}
+
+#Run loops/vector functions to get picture size
+new.photos<-lapply(photos, unlist(avg.size))
+avg.photo<-sapply(sapply(new.photos, as.numeric), mean)
+
+new.photos<-list()
+new.photos[1:1000]<-lapply(photos[1:1000], unlist(avg.size))
+
+
+
+#avg.photos<-rep(0, length(photos))
+#for (i in 1:length(photos)){
+#  size<-as.numeric(sapply(unlist(photos[i]), file.size))
+#  avg.size<-sum(size)/length(photos[i])
+#  avg.photos[i]<-avg.size
+#  print(paste("Finished listing", i))
+#}
+
